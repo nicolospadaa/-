025cf7bf79d5ba617496b93a8d620b7628009b7e
nicolospadaa <144077810+nicolospadaa@users.noreply.github.com>
2024-09-04 17:31:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ const require = createRequire(__dirname);
 const {say} = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
 
-say('The Mystic\nBot', {
+say('Lerox\nBot', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']});
-say(`Bot creado por Bruno Sobrino`, {
+say(`Bot creado por Nicolò Spada`, {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta']});
